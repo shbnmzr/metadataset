c22b876c205ddba6314748704b184df5150ae1e0
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2025-11-26 15:46:12 +0100

    something done!

--- metadataset/download/splits.py
+++ metadataset/download/splits.py
@@ -1,3 +1,8 @@
 from pathlib import Path
 import logging
 from typing import List, Tuple
+
+from metadataset.download.fetcher import (download_genome_file)
+
+def save_paths(paths: List[str], out_file: Path) -> None:
+    out_file.write_text('\n'.join(paths))
