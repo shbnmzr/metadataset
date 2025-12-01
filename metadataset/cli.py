141daacf010b36d7a3a9eb3962a14abea7e7c852
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2025-12-01 20:46:37 +0100

    something done!

--- metadataset/cli.py
+++ metadataset/cli.py
@@ -1,2 +1,37 @@
 
 from metadataset.download.manager import download_split
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='metadataset',
+        description='Meta-dataset download and preparation toolkit'
+    )
+
+    subparsers = parser.add_subparsers(dest='command')
+
+    # download command
+    dl = subparsers.add_parser('download', help='Download genomes from GenBank')
+
+    dl.add_argument('--category', required=True, help='Genome category')
+    dl.add_argument('--base_dir', required=True, help='Dataset directory')
+    dl.add_argument('--train_cutoff', required=True, help='Dataset training cutoff in the format DD-MM-YYYY')
+    dl.add_argument('--val_cutoff', required=True, help='Dataset validation cutoff in format DD-MM-YYYY')
+    dl.add_argument('--test_end', required=True, help='Dataset test cutoff in format DD-MM-YYYY')
+
+    dl.add_argument('--assembly_level',
+                    default='Complete Genome',
+                    help='Comma-separated list of allowed assembly levels')
+
+    dl.add_argument('--seed', type=int, default=None, help='Random seed')
+
+    args = parser.parse_args()
+
+    if args.command == 'download':
+        download_split(args.category, args.base_dir, args.train_cutoff, args.val_cutoff,)
+    else:
+        parser.print_help()
+
+
+if __name__=='__main__':
+    main()
